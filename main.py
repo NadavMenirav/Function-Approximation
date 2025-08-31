17a37434bc888965985576ee45444f798428c421
Nadav Menirav <nmenirav@gmail.com>
2025-09-01 01:07:52 +0300

    (Feat) adding the x and y values to the array

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import numpy as np
 
 def f(x):
+    # Input: a real number x
+    # Output: the value of f(x)
     return 1 / (1 + x ** 2)
+
+
 def main():
     # This function generates a polynomial of a degree n that will be an approximation of
     # the function f(x) = 1/(x^2+1)
 
-    N = 5
+    N = 5 # The degree of the polynomial
+    x = [] # Array of the x values
+    y = [] # Array of the y values
+
+    for k in range(N):
+        x.append(2 * k - 1)
+        y.append(f(x[k]))
+
 
 if __name__ == '__main__':
     main()
