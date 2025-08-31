208e69f1cda4ed855613519b1581cca16e79281d
Nadav Menirav <nmenirav@gmail.com>
2025-09-01 01:35:55 +0300

    (Feat) final result

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     # This function generates a polynomial of a degree n that will be an approximation of
     # the function f(x) = 1/(x^2+1)
 
-    N = 5 # The degree of the polynomial
+    N = 15 # The degree of the polynomial
 
     k = np.arange(0, N + 1)
     x = 2 * k / N - 1 # Array of x values
@@ -19,6 +19,7 @@ def main():
     V = np.vander(x, increasing = True) # The vandermonde matrix
 
     a = np.linalg.solve(V, y) # Solving for the coefficients
+    print(a)
 
 if __name__ == '__main__':
     main()
